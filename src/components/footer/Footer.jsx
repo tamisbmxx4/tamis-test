a7de6218a18397e769d56dcc9d49ee821e2cdba9
mistafk <105982996+tamisbmxx4@users.noreply.github.com>
2022-06-30 23:48:05 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.scss';
 
 import { Link } from 'react-router-dom';
 
-import bg from '../../assets/footer-bg.jpeg';
+import bg from '../../assets/footer-bg.JPG';
 import logo from '../../assets/tmovie.png';
 
 const Footer = () => {
